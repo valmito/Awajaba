65b8e737da3c362437120a82c7f90f3019758749
SANGLIER <valmont.sanglier.sio@gmail.com>
2019-10-08 15:16:59 +0200

    Classe Repas[attr.SANGLIER]

---
+++ Repas.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Repas {
+	private int numéro;
+	private LocalDate date;
+	
+	public Repas(int numero,LocalDate date){
+		this.numéro=numero;
+		this.date=date;
+	}
+}
---
+++ test.java
@@ -0,0 +1,1 @@
+System.out.println("Ceci est la version 1.1 du fichier test de AwajabaLC12\nN'y touchez pas s'il vous plaît")
